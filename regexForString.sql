2ca6c01cfeeed20683f527acdd27beab05ddcb0e
Abhishek Mahadik <abmahadik2014@gmail.com>
2023-04-04 15:38:47 +0530

    SQL Query Using REGEXP

---
+++ regexForString.sql
@@ -0,0 +1,11 @@
+/* Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+Your result cannot contain duplicates.*/
+select distinct CITY 
+from STATION
+where CITY REGEXP '(a|e|i|o|u)$';
+
+--Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+--Your result cannot contain duplicates.
+select distinct CITY
+from STATION
+where CITY REGEXP '^(a|e|i|o|u)';
