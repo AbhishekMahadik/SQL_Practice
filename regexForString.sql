e869e3584b5687f63b0fbf9d3d5641399d73c3b5
Abhishek Mahadik <abmahadik2014@gmail.com>
2023-04-05 21:12:52 +0530

    New Question For practice

--- regexForString.sql
+++ regexForString.sql
@@ -4,8 +4,32 @@ select distinct CITY
 from STATION
 where CITY REGEXP '(a|e|i|o|u)$';
 
+select distinct city from station where right(city,1) in ('a','e','i','o','u');
+SELECT DISTINCT city FROM station WHERE SUBSTR(city, -1) in ('a','e','i','o','u') ;
+
 --Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
 --Your result cannot contain duplicates.
 select distinct CITY
 from STATION
 where CITY REGEXP '^(a|e|i|o|u)';
+
+--Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+select distinct CITY 
+from STATION
+where CITY REGEXP '^(a|e|i|o|u).*(a|e|i|o|u)$';
+
+--Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+select distinct CITY  from STATION where CITY REGEXP '^[^aeiou]';
+
+select distinct CITY from STATION where substring(CITY,1,1) not in ('a','e','i','o','u');
+select distinct CITY from STATION where LEFT(City, 1) not in ('a','e','i','o','u');
+SELECT DISTINCT city FROM station WHERE NOT SUBSTR(city,1,1) IN ('A','E','I','O','U');
+
+--Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+select distinct CITY from STATION where substr(CITY,-1) not in ('a','e','i','o','u');
+select distinct CITY from STATION where CITY REGEXP '[^aeiou]$';
+
+--Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.	
+select distinct CITY  from STATION where CITY REGEXP '^[^a|e|i|o|u]' or CITY REGEXP '[^a|e|i|o|u]$';
+select distinct CITY from STATION where LEFT(City,1) not in ('a','e','i','o','u') 
+or RIGHT(city,1) NOT IN ('a','e','i','o','u');
